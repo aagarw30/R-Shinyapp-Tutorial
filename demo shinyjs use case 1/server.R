ad15a8acfb74654f8a9be7f08b2017cbb7fb5d6e
aagarw30 <aagarw30@gmail.com>
2018-02-19 20:48:55 +0530

    added new files

--- demo shinyjs use case 1/server.R
+++ demo shinyjs use case 1/server.R
@@ -1,19 +1,22 @@
 library(shiny)
 library(shinyjs)
+# install.packages("data.table")
 library(data.table) # to use fread function for faster data read from URL
+
 
 shinyServer(function(input, output, session){
 
   # reading data from a given UCI URL
   URL = "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
   mydata = fread(URL, sep = ",", showProgress = FALSE) # reading data from URL
-
+  # fread() is from data.table package
   
   ## hide the text message and enable the save button only when data is read from URL
   if(is.null(mydata)) {return()}
   else
     { hide("p1");enable("save")}
   
+  # hide() & enable() are shinyjs functions
   
   ## On click on the save button , read dataset from uci URL and write to working directory
   observeEvent(
@@ -22,12 +25,12 @@ shinyServer(function(input, output, session){
     { 
       
     starttime = Sys.time() 
-    write.csv(dat, "data.csv", row.names = FALSE) # writing the csv
+    write.csv(mydata, "data.csv", row.names = FALSE) # writing the csv
     endtime = Sys.time()
     timelapse = endtime - starttime # calculate time taken in writing the file
     ms = timelapse *1000 # converting to milliseconds
     delay(ms, alert("File saved, thanks for using the app")) # add delay (until file is written) and then throw the alert message
-    
+    # delay() is shinyjs function
     }
     
   )
--- demo shinyjs use case 1/ui.R
+++ demo shinyjs use case 1/ui.R
@@ -4,12 +4,17 @@ library(shinyjs)
 shinyUI(fluidPage(
   h3("Demo - shinyjs alert(), delay(), hide(), enable() functions together with a simple data read and write use case"),
   h4(" First the data will be read from UCI URL. While the data is being read, a text message will appear telling user that data is being read. "),
-  h4("Also, the save button is disabled.  Once data read is completed, the text message shall go away and save button will enable"),
+  h4("Also, the save button is disabled.  Once data read is completed, the text message shall go away and save button will enable. Data will be written to the working directory locally and once completed an alert message will show up saying file saved."),
+  
+  
   useShinyjs(), ## initializing for Shiny to be able use the functions that comes with shinyjs package.
   hr(),
-  ## Save button will be disabled on the page load
-  tags$div(id="p1", h4("Reading the data from web...."), style='color:brown;'),
-  disabled(actionButton("save" , "Save Dataset")) # for writing the file to working directory
   
+  # define the ID for heading h4() using div tag
+  tags$div(id="p1", h4("Reading the data from web...."), style='color:brown;'),
+  
+  ## Save button will be disabled on the page load
+  disabled(actionButton("save" , "Save Dataset")) # for writing the file to working directory
+  # disabled() is shinyjs function
   
 ))
